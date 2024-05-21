e852d55e76f3bb980691595255d3a24a4e35ff47
Suman-A1 <sumanpervaiz3@gmail.com>
2024-05-22 00:18:50 +0500

    trying to seperate modal

--- src/pages/user/UserProfile.jsx
+++ src/pages/user/UserProfile.jsx
@@ -134,6 +134,12 @@ const UserProfile = () => {
     // console.log(file);
     // setImage(event.target.files[0]);
   };
+  const data = {
+    handleChange: handleChange,
+    handlePasswordSubmit: handlePasswordSubmit,
+    passwordErrors: passwordErrors,
+    formData: formData,
+  };
 
   return (
     <>
@@ -347,96 +353,11 @@ const UserProfile = () => {
           </form>
         </div>
       </div>
-      <Modal isVisible={showModal} onClose={() => setShowModal(false)}>
-        <div className="mt-8 px-8">
-          <h1 className="font-bold text-2xl leading-10">Change Password</h1>
-          <p className="font-normal text-sm mt-4">Enter Your Password</p>
-        </div>
-        <div className="px-8">
-          <form onSubmit={handlePasswordSubmit}>
-            <div className="relative mt-10">
-              <div className="border-[1px] rounded-3xl -top-[14px] left-3 absolute px-3 bg-white flex items-center justify-center">
-                <label
-                  htmlFor="currentPassword"
-                  className="font-normal py-1 text-xs"
-                >
-                  Current Password
-                </label>
-              </div>
-              <input
-                type="password"
-                id="currentPassword"
-                name="currentPassword"
-                value={formData.currentPassword}
-                onChange={handleChange}
-                placeholder="Password"
-                className="border-2 py-3 px-4 w-full rounded-xl focus-within:border-indigo-300 focus-within:ring focus-within:ring-indigo-200 focus-within:ring-opacity-50 outline-none"
-              />
-              {passwordErrors.currentPassword && (
-                <p className="text-red-500 text-xs">
-                  {passwordErrors.currentPassword}
-                </p>
-              )}
-            </div>
-            <div className="relative mt-[30px]">
-              <div className="border-[1px] rounded-3xl -top-[14px] left-3 absolute px-3 bg-white flex items-center justify-center">
-                <label
-                  htmlFor="newPassword"
-                  className="font-normal py-1 text-xs"
-                >
-                  New Password
-                </label>
-              </div>
-              <input
-                type="password"
-                id="newPassword"
-                name="newPassword"
-                value={formData.newPassword}
-                onChange={handleChange}
-                placeholder="Password"
-                className="border-2 py-3 px-4 w-full rounded-xl focus-within:border-indigo-300 focus-within:ring-indigo-200 focus-within:ring-opacity-50 outline-none"
-              />
-              {passwordErrors.newPassword && (
-                <p className="text-red-500 text-xs">
-                  {passwordErrors.newPassword}
-                </p>
-              )}
-            </div>
-            <div className="relative mt-[30px]">
-              <div className="border-[1px] rounded-3xl -top-[14px] left-3 absolute px-3 bg-white flex items-center justify-center">
-                <label
-                  htmlFor="confirmPassword"
-                  className="font-normal py-1 text-xs"
-                >
-                  Confirm New Password
-                </label>
-              </div>
-              <input
-                type="password"
-                id="confirmPassword"
-                name="confirmPassword"
-                value={formData.confirmPassword}
-                onChange={handleChange}
-                placeholder="Password"
-                className="border-2 py-3 px-4 w-full rounded-xl focus-within:border-indigo-300 focus-within:ring focus-within:ring-indigo-200 focus-within:ring-opacity-50 outline-none"
-              />
-              {passwordErrors.confirmPassword && (
-                <p className="text-red-500 text-xs">
-                  {passwordErrors.confirmPassword}
-                </p>
-              )}
-            </div>
-            <div className="grid mt-8 mb-10">
-              <button
-                type="submit"
-                className="bg-green-500 text-white text-lg font-normal rounded-xl py-2 hover:bg-green-700"
-              >
-                Update
-              </button>
-            </div>
-          </form>
-        </div>
-      </Modal>
+      <Modal
+        isVisible={showModal}
+        onClose={() => setShowModal(false)}
+        data={data}
+      />
     </>
   );
 };
